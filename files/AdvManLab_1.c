e6da49334a5571eb341a2a76d87d9d166b65e7f3
xumiao <xumiao@umich.edu>
2018-12-12 09:37:46 -0500

    add modified example

---
+++ files/AdvManLab_1.c
@@ -0,0 +1,68 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include "common_structs.h"
+
+//
+// global vars, e.g., global tags
+//
+bool Camera2_Inspect = 0;
+bool PPX2 = 0;
+bool RFIDTP2 = 0;
+bool Mode_Start = 0;
+bool kk = 1;
+bool Mode_Semi = 0;
+int var;
+
+void Cell_1(bool k) {
+  // function body omitted for simplicity
+  if  (kk) {
+    v = v + 1;
+    k = 0;
+  }
+}
+
+void Cell_2(int input) {
+  //
+  // a local var, e.g., a local tag
+  //
+  TIMER Cam2;
+  Cam2.DN = 0;
+  Cam2.EN = 0;
+  Cam2.TT = 0;
+
+  int i = 0;
+
+  while (i++ < 5) {
+    printf("Loop #%d\n", i);
+
+    if (Mode_Start || Mode_Semi) {
+      if ((PPX2 && RFIDTP2) || Cam2.TT) {
+        if (!Cam2.DN) {
+          Camera2_Inspect = 1;
+          // enable timer
+          Cam2.EN = 1;
+          Cam2.TT = 1;
+          Cam2.PRE = 3000;
+          Cam2.ACC = 0;
+        }  
+      }
+    }
+    if (Cam2.EN) {
+      Cam2.ACC + = 2000;
+      Cam2.DN = Cam2.ACC >= Cam2.PRE;
+    }
+
+    if (Mode_Start || Mode_Semi) {
+      if (Cam2.DN) {
+        // when will it be 1?
+        Camera2_Inspect = 1 + input;
+      }
+    }
+  }
+}
+
+int main() {
+  var = 100;
+  Cell_1();
+  Cell_2(var);
+}
